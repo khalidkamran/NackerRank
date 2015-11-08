c3175b9377d807abac996a0537b80648c1aaad1b
Kamran Khalid <khalidkamran@hotmail.com>
2015-11-09 01:23:30 +0500

    junit test
    Change-Id: I1c9d0ba84f6345ff382685a9ea0541b29a62b01a

--- NackerRank/src/test/FunnyStringTest.java
+++ NackerRank/src/test/FunnyStringTest.java
@@ -2,13 +2,23 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
-public class FunnyStringTest {
+import StringManipulation.FunnyString;
 
+
+public class FunnyStringTest {
+	private FunnyString fString;
+	
+	@Before
+	public void initilize(){
+		fString = new FunnyString();
+	}
+	
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		assertArrayEquals("is funny failed", "", fString.isFunny("ABXZ"));
 	}
 
 }
